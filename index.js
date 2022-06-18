f0a78f39f34996b59d9bf9479f1b804b15572e40
sayed zaid <szazid444666@gmail.com>
2022-06-19 00:20:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+AOS.init();
+
+
+
+// progress circle js
+
+let options = {
+  startAngle: -1.55,
+  size: 150,
+  value: 0.85,
+  fill: { gradient: ["#a445b2", "#fa4299"] },
+};
+$(".circle .bar")
+  .circleProgress(options)
+  .on("circle-animation-progress", function (event, progress, stepValue) {
+    $(this)
+      .parent()
+      .find("span")
+      .text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+$(".html .bar").circleProgress({
+  value: 0.8,
+});
+$(".js .bar").circleProgress({
+  value: 0.5,
+});
+$(".laravel .bar").circleProgress({
+  value: 0.8,
+});
+
+// landing page js
+
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+// testimonial js
+
+("use strict");
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(
+    document.getElementById("testim-dots").children
+  ),
+  testimContent = Array.prototype.slice.call(
+    document.getElementById("testim-content").children
+  ),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 10000,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+window.onload = function () {
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < testimDots.length; k++) {
+      testimContent[k].classList.remove("active");
+      testimContent[k].classList.remove("inactive");
+      testimDots[k].classList.remove("active");
+    }
+
+    if (slide < 0) {
+      slide = currentSlide = testimContent.length - 1;
+    }
+
+    if (slide > testimContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+
+    if (currentActive != currentSlide) {
+      testimContent[currentActive].classList.add("inactive");
+    }
+    testimContent[slide].classList.add("active");
+    testimDots[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide((currentSlide += 1));
+    }, testimSpeed);
+  }
+
+  testimLeftArrow.addEventListener("click", function () {
+    playSlide((currentSlide -= 1));
+  });
+
+  testimRightArrow.addEventListener("click", function () {
+    playSlide((currentSlide += 1));
+  });
+
+  for (var l = 0; l < testimDots.length; l++) {
+    testimDots[l].addEventListener("click", function () {
+      playSlide((currentSlide = testimDots.indexOf(this)));
+    });
+  }
+
+  playSlide(currentSlide);
+};
